ddbfca5e28c32c6be63f9c7bcfac0b2dd5b8e964
Al-Arsenault201 <69997839+Al-Arsenault201@users.noreply.github.com>
2026-02-04 16:22:08 -0500

    Add files via upload

---
+++ Classwork and Projects/cw2_starter_code.py
@@ -0,0 +1,17 @@
+# Name : Alice Smith ( replace Alice's name with your name !)
+# Part 1: Prompt the user for 4 words : a noun , a verb , an adjective ,
+# and a place . Then , display the words as a sentence .
+noun = ???
+verb = ???
+adj = ???
+place = ???
+print (" Bring your " + ???)
+# Part 2: As of July 2024 , the Voyager 1 spacecraft is about
+# 24 ,447 ,732 ,226 kilometers from Earth . Radio transmissions are about
+# the speed of light : 300 ,000 kilometers per second . Write code which
+# calculates the estimated transmission time for one -way communication
+# with Voyager 1 in hours . Your answer should be approximately 22.6 hours .
+distance_over_speed = 24447732226.0/300000.0 #this calculates the number of seconds it takes
+# Now you need to convert from seconds to hours
+
+# Now print the result and include a description
