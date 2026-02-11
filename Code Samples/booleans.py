88e29c62e0d37df3c94222353d6e4de09e49a931
Al-Arsenault201 <69997839+Al-Arsenault201@users.noreply.github.com>
2026-02-11 18:16:20 -0500

    Add files via upload

---
+++ Code Samples/booleans.py
@@ -0,0 +1,23 @@
+# coding from February 11
+
+# using Boolean variables
+"""
+x = True
+y = False
+
+z = not x
+
+print(x)
+print(y)
+print(z)
+
+print ("x or y",x or y)
+print ("x and y", x and y)
+"""
+
+age = 16
+if age >= 18:
+    print("yes, you can vote")
+else:
+    print("try again in the next election")
+    
---
+++ Code Samples/february_11_coding.py
@@ -0,0 +1,16 @@
+#in-class coding from Wednesday February 11 2026
+"""
+import math
+
+print(math.pi)
+pi = 3
+print ("pi is:", pi)
+print(math.sqrt(math.pi))
+
+"""
+
+import math as m
+print (m.pi)
+
+print(m.sqrt(m.pi))
+
